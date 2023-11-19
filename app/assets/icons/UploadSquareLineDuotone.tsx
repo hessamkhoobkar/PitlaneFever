bc73e6eb9937e0cdd817e312f26b5090dbad7dab
hessam_khoobkar <amirhessam.dev@gmail.com>
2023-11-20 00:35:08 +0330

    Add PostingCard component with user profile and post functionality

---
+++ app/assets/icons/UploadSquareLineDuotone.tsx
@@ -0,0 +1,24 @@
+export function UploadSquareLineDuotone(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <g fill="none" stroke="currentColor" strokeWidth="1.5">
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M12 17v-7m0 0l3 3m-3-3l-3 3"
+        ></path>
+        <path strokeLinecap="round" d="M16 7H8"></path>
+        <path
+          d="M2 12c0-4.714 0-7.071 1.464-8.536C4.93 2 7.286 2 12 2c4.714 0 7.071 0 8.535 1.464C22 4.93 22 7.286 22 12c0 4.714 0 7.071-1.465 8.535C19.072 22 16.714 22 12 22s-7.071 0-8.536-1.465C2 19.072 2 16.714 2 12Z"
+          opacity=".5"
+        ></path>
+      </g>
+    </svg>
+  );
+}
--- app/components/PostingCard.tsx
+++ app/components/PostingCard.tsx
@@ -1,67 +1,142 @@
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
+"use client";
+
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+
 import { Paperclip } from "../assets/icons/Paperclip";
+import { UploadSquareLineDuotone } from "../assets/icons/UploadSquareLineDuotone";
 
-export default async function PostingCard() {
-  const cookieStore = cookies();
-  const supabase = createServerComponentClient<Database>({
-    cookies: () => cookieStore,
+type Profile = Database["public"]["Tables"]["profiles"]["Row"];
+
+export default function PostingCard() {
+  // Set skeloton for card
+  const router = useRouter();
+
+  const [currentUser, setCurrentUser] = useState<Profile | null>(null);
+  const [newPost, setNewPost] = useState("");
+  const [submitting, setSubmitting] = useState(false);
+
+  const supabase = createClientComponentClient<Database>();
+
+  async function getUser() {
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+
+    return user;
+  }
+
+  async function getProfile() {
+    const user = await getUser();
+
+    if (user) {
+      const response = await supabase
+        .from("profiles")
+        .select("*")
+        .eq("user_connect", user.id);
+
+      if (response.data) {
+        setCurrentUser(response.data[0]);
+      }
+    }
+  }
+
+  async function handleInsertReq() {
+    if (!currentUser || newPost === "") {
+      // TODO: Add error message
+      return;
+    }
+
+    setSubmitting(true);
+
+    const { data, error } = await supabase
+      .from("posts")
+      .insert([
+        {
+          author_id: currentUser?.id || "",
+          post: newPost,
+        },
+      ])
+      .select();
+
+    console.log(data && { data });
+    console.log(error && { error });
+
+    setNewPost("");
+    setSubmitting(false);
+    data && router.refresh();
+  }
+
+  useEffect(() => {
+    getProfile();
+
+    return () => {
+      getProfile();
+    };
   });
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
-  if (!user) {
-    return null;
-    // TODO: I should redirect to login page
-  }
-
-  const { data: profile } = await supabase
-    .from("profiles")
-    .select("*")
-    .eq("user_connect", user.id);
-
-  if (!profile) {
-    return null;
-    // TODO: I should write error for failed user fetch
-  }
-
-  const currentUser = profile && profile[0];
 
   return (
     <div className="w-full rounded-2xl bg-secondary flex justify-start items-start gap-8 p-6 pb-2">
-      <div>
-        <Image
-          className="w-16 h-16 rounded-2xl"
-          width={64}
-          height={64}
-          src={currentUser.avatar_url}
-          alt={currentUser.name}
-        />
-      </div>
-      <div className="flex flex-col justify-start items-start grow gap-1">
-        <div className="flex justify-start items-center gap-2">
-          <span>{currentUser.name}</span>
-          <span className="text-write text-xs opacity-60">
-            @{currentUser.username}
-          </span>
-        </div>
-        <textarea
-          className="bg-secondary w-full rounded-2xl p-2 hover:bg-neutral focus:bg-neutral focus:ring-0 focus:outline-none"
-          name="post"
-          id="post"
-          rows={4}
-        />
-        <div className="w-full flex justify-between items-center">
-          <button className="btn btn-circle btn-ghost text-lg -ms-6">
-            <Paperclip />
-          </button>
-          <button className="btn text-primary bg-secondary border-secondary">
-            Post
-          </button>
-        </div>
-      </div>
+      {currentUser ? (
+        <>
+          <div>
+            <Image
+              className="w-16 h-16 rounded-2xl"
+              width={64}
+              height={64}
+              src={currentUser.avatar_url}
+              alt={currentUser.name}
+            />
+          </div>
+          <div className="flex flex-col justify-start items-start grow gap-1">
+            <div className="flex justify-start items-center gap-2">
+              <span>{currentUser.name}</span>
+              <span className="text-write text-xs opacity-60">
+                @{currentUser.username}
+              </span>
+            </div>
+            <textarea
+              className="bg-secondary w-full rounded-2xl p-2 hover:bg-neutral focus:bg-neutral focus:ring-0 focus:outline-none"
+              name="post"
+              id="post"
+              rows={4}
+              value={newPost}
+              onChange={(e) => setNewPost(e.target.value)}
+            />
+            <div className="w-full flex justify-between items-center">
+              <button className="btn btn-circle text-write bg-secondary border-transparent hover:border-transparent hover:text-primary hover:bg-primary hover:bg-opacity-5 text-lg cursor-not-allowed">
+                <Paperclip />
+              </button>
+              <button
+                className="btn text-primary bg-secondary border-transparent hover:bg-primary hover:bg-opacity-5 hover:border-transparent capitalize min-w-[12rem]"
+                onClick={handleInsertReq}
+              >
+                {submitting ? (
+                  <>
+                    <UploadSquareLineDuotone className="text-2xl animate-ping" />
+                    <span>Sharing ...</span>
+                  </>
+                ) : (
+                  <>
+                    <UploadSquareLineDuotone className="text-2xl" />
+                    <span>Share your thought</span>
+                  </>
+                )}
+              </button>
+            </div>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="w-16 h-16 rounded-2xl bg-neutral"></div>
+          <div className="flex flex-col justify-start items-start grow gap-2 pb-4">
+            <div className="bg-neutral w-full h-8 rounded"></div>
+            <div className="bg-neutral w-full h-32 rounded-lg"></div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
